d603b6d8638cf56e5766c66fea3d91d8c1e68ab1
beginhope <109449506+beginhope@users.noreply.github.com>
2022-07-17 14:13:05 +0900

    end of 2525

--- 코딩.cpp
+++ 코딩.cpp
@@ -1,10 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
 int main(){
-	printf("Hello world!");
+    
+int h;
+int m;
+int t;
+	
+scanf("%d %d %d", &h, &m, &t);
+    
+h += t / 60;
+m += t % 60;
+
+if(m>=60){
+    h += 1;
+    m -= 60;
+}
+
+if(h>=24){
+    h -= 24;
+}
+    
+printf("%d %d", h, m);
 
 return 0;
 }
-
-
 
 
